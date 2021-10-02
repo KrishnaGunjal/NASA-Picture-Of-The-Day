770a655149da29bd9248f12124196ed28ab7b1e6
Krishna Gunjal <kgunjal51@gmail.com>
2021-10-02 15:43:22 +0530

    Created project.

---
+++ NASA Picture of The Day/NASA Picture of The DayTests/NASA_Picture_of_The_DayTests.swift
@@ -0,0 +1,33 @@
+//
+//  NASA_Picture_of_The_DayTests.swift
+//  NASA Picture of The DayTests
+//
+//  Created by Krishna Gunjal on 02/10/21.
+//
+
+import XCTest
+@testable import NASA_Picture_of_The_Day
+
+class NASA_Picture_of_The_DayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
