a5897b01e194124bf4ae5f9a1e224d8afb498e38
Krishna Gunjal <kgunjal51@gmail.com>
2021-10-02 16:34:02 +0530

    Added UI.

--- NASA Picture of The Day/NASA Picture of The Day/ViewController.swift
+++ NASA Picture of The Day/NASA Picture of The Day/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topicTitle: UILabel!
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var credits: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pictureDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
